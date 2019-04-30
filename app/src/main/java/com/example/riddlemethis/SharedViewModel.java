12f95cec271343d0740dd5070f09c37500bd988b
Isaiah <tscephalos@gmail.com>
2019-04-30 07:19:30 -0700

    Started on Login fragment.

--- app/src/main/java/com/example/riddlemethis/SharedViewModel.java
+++ app/src/main/java/com/example/riddlemethis/SharedViewModel.java
@@ -2,6 +2,17 @@ package com.example.riddlemethis;
 
 import androidx.lifecycle.ViewModel;
 
-public class SharedViewModel extends ViewModel {
+import com.backendless.BackendlessUser;
 
+public class SharedViewModel extends ViewModel {
+    private BackendlessUser loggedInUser;
+
+
+    public BackendlessUser getLoggedInUser() {
+        return loggedInUser;
+    }
+
+    public void setLoggedInUser(BackendlessUser loggedInUser) {
+        this.loggedInUser = loggedInUser;
+    }
 }
