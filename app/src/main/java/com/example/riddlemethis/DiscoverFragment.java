e1ae7a4a211d3ff1f84cfa4cfe064c8e9e9621f4
Isaiah <tscephalos@gmail.com>
2019-06-02 01:48:44 -0700

    Did discover fragment.

--- app/src/main/java/com/example/riddlemethis/DiscoverFragment.java
+++ app/src/main/java/com/example/riddlemethis/DiscoverFragment.java
@@ -6,15 +6,34 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+import com.backendless.persistence.LoadRelationsQueryBuilder;
+
+import java.util.Collections;
+import java.util.List;
 
 
 public class DiscoverFragment extends Fragment {
 
+    private View rootView;
+    private ListView listView;
+    private ProgressBar progressBar;
+
     private SharedViewModel model;
+    private List<Riddle> riddles;
+    private List<Riddle> savedRiddles;
 
     public DiscoverFragment() {
         // Required empty public constructor
@@ -31,7 +50,71 @@ public class DiscoverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false);
+        rootView = inflater.inflate(R.layout.fragment_discover, container, false);
+        wireWidgets(rootView);
+        listView.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+        setListeners();
+        return rootView;
     }
 
+    private void setListeners() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Riddle riddle = riddles.get(position);
+                Bundle args = new Bundle();
+                args.putParcelable("riddle", riddle);
+                args.putBoolean("isDiscovered", true);
+                Navigation.findNavController(view).navigate(R.id.action_discoverFragment_to_viewRiddleFragment,
+                        args);
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Backendless.Data.of(Riddle.class).find(new AsyncCallback<List<Riddle>>() {
+            @Override
+            public void handleResponse(List<Riddle> response) {
+                Collections.shuffle(response);
+                riddles = response;
+                populateListView();
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+
+            }
+        });
+    }
+
+    private void wireWidgets(View rootView) {
+        listView = rootView.findViewById(R.id.listview_discover_list);
+        progressBar = rootView.findViewById(R.id.progressBar_discover_loadingBar);
+    }
+
+    private void populateListView(){
+        LoadRelationsQueryBuilder<Riddle> loadRelationsQueryBuilder;
+        loadRelationsQueryBuilder = LoadRelationsQueryBuilder.of(Riddle.class);
+        loadRelationsQueryBuilder.setRelationName("savedRiddles");
+
+        Backendless.Data.of(BackendlessUser.class).loadRelations(Backendless.UserService.loggedInUser(),
+                loadRelationsQueryBuilder, new AsyncCallback<List<Riddle>>() {
+                    @Override
+                    public void handleResponse(List<Riddle> response) {
+                        savedRiddles = response;
+                        listView.setAdapter(new DiscoveredRiddlesAdapter(getContext(),
+                                R.layout.listitem_discoverriddles_riddle, riddles, savedRiddles));
+                        progressBar.setVisibility(View.INVISIBLE);
+                        listView.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+
+                    }
+                });
+    }
 }
